ee9b2fab4dccb3c08d00e7ad44f7ef1523df27c2
hasan-yamin <hasanyamin12@gmail.com>
2022-11-24 23:08:44 +0300

    maps

--- ts/main.ts
+++ ts/main.ts
@@ -33,3 +33,113 @@ btns.forEach((btn) => {
         btn.classList.add('active')
     })
 })
+
+// feedback
+let feedbacks: {
+    imgSrc: string;
+    position: string;
+    name: string;
+    desc: string;
+}[] = [
+        {
+            imgSrc: "../imgs/home1.png",
+            position: "CEO at Google",
+            name: "Harriet Maxwell",
+            desc: "Do you want to be even more successful? Learn to love learning and growth. The more effort you put into improving your skills, the bigger the payoff you."
+        },
+        {
+            imgSrc: "../imgs/home1.png",
+            position: "facebook",
+            name: "k Maxwell",
+            desc: "Do you want to be even more successful? Learn to love learning and growth. The more effort you put into improving your skills, the bigger the payoff you."
+        },
+        {
+            imgSrc: "../imgs/home1.png",
+            position: "freelance",
+            name: "Hasan",
+            desc: "Do you want to be even more successful? Learn to love learning and growth. The more effort you put into improving your skills, the bigger the payoff you."
+        }
+    ]
+
+show_feedbacks(feedbacks);
+changeCards()
+function show_feedbacks(feedbacks: any[]) {
+    // adding first 10 profiles on the page
+    let feedbackCards: HTMLDivElement | null = <HTMLDivElement>document.getElementById("feedback-cards");
+    feedbackCards.innerHTML = ''
+    // console.log("feedbacks.length : " + feedbacks.length);
+    // Show the first 10 profiles on the page
+    for (let i = 0; i < feedbacks.length; i++) {
+        let cardContent: string = `
+            <div class="pic"><img src="${feedbacks[i].imgSrc}"></div>
+            <div class="disc">
+                <p>${feedbacks[i].desc}</p>
+                <h3>${feedbacks[i].name}</h3>
+                <span>${feedbacks[i].position}</span>
+            </div> `
+        let card: HTMLDivElement = document.createElement('div')
+        card.classList.add("card")
+        card.innerHTML = cardContent
+        feedbackCards.appendChild(card);
+        if (i === 1)
+            break
+    }
+}
+function changeCards() {
+    const myTimeout = setTimeout(function () {
+        // shift feedbacks array 1 element backward
+        feedbacks.push(feedbacks.shift());
+        // update feeds on screen
+        show_feedbacks(feedbacks);
+        changeCards()
+    }, 3000)
+}
+
+
+// next and previous feeds
+let pervFeed: HTMLDivElement | null = <HTMLDivElement>document.getElementById("prev-feed");
+let nextFeed: HTMLDivElement | null = <HTMLDivElement>document.getElementById("next-feed");
+// add event to next and previous buttons
+nextFeed.addEventListener('click', function () {
+    // shift feedbacks array 1 element backward
+    feedbacks.push(feedbacks.shift());
+    // update feeds on screen
+    show_feedbacks(feedbacks);
+});
+pervFeed.addEventListener('click', function () {
+    // shift feedbacks array 1 element forward
+    feedbacks.unshift(feedbacks.pop());
+    // update feeds on screen
+    show_feedbacks(feedbacks);
+});
+
+
+// map
+
+// Initialize and add the map
+function initMap(): void {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.031 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(
+        document.getElementById("map") as HTMLElement,
+        {
+            zoom: 4,
+            center: uluru,
+        }
+    );
+
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+}
+
+declare global {
+    interface Window {
+        initMap: () => void;
+    }
+}
+window.initMap = initMap;
+
